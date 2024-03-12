79246a362e7391d002de294bb4c867688e5d6af7
Tim <tima.uramov@yandex.ru>
2024-03-12 13:24:58 +0300

    add in old

--- lab_1/old.py
+++ lab_1/old.py
@@ -17,14 +17,31 @@ def search_index(list, number):
     return 'Нет данного числа'
 
 def deleted_el(lisr, element):
+    start_time = time.perf_counter()
     if element in list:
         list.remove(element)
-    return list
+        end_time = time.perf_counter()
+        total_time = (end_time - start_time) * 1000
+    return list, total_time
 
 def deleted_ell(list, element):
+    start_time = time.perf_counter()
     for i in range(0, len(list)):
         if list[i] == element:
             list.remove(element)
+            end_time = time.perf_counter()
+            total_time = (end_time - start_time) * 1000
+            return list, total_time
+
+def insert_element(lst, element):
+    start_time = time.perf_counter()
+    lst.append(element)
+    lst.sort()
+    end_time = time.perf_counter()
+    total_time = (end_time - start_time) * 1000
+    return lst, total_time
+
+
 
 size = int(input('Введите размер случайного массива: '))
 list = generation_list(size)
@@ -33,3 +50,5 @@ number = int(input('Введите число для поиска: '))
 print('Индекс данного числа: ', search_index(list, number))
 element = int(input('Введите число, которое хотите удалить: '))
 print('Ваш массив ', deleted_el(list, element))
+element = int(input('Введите число, которое хотите вставить: '))
+print('Ваш массив: ', insert_element(list, element))
